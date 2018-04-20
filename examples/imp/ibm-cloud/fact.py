f60000053fb08237c9f6447ba9c730eef70ea946
Christiano Braga <cbraga@ic.uff.br>
2018-04-20 16:42:03 -0300

    Removing ibm-cloud tests from master.

--- examples/imp/ibm-cloud/fact.py
+++
@@ -1,9 +0,0 @@
-y = 1
-def fact(x) :
-      while not(x == 0) :
-         global y
-         y = y * x
-         x = x - 1
-      z = y
-      y = 1
-      return z
--- examples/imp/ibm-cloud/test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-port = os.getenv('PORT', 8000)
-
-print(port)
