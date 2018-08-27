759b8dcdc269215c415d81b75b16b306b458b9d7
Fernando Jose Da Silva Mendes <fernando.jose@b2wdigital.com>
2018-08-27 18:53:56 -0300

    Para continuar...

--- python/llvm_pi.py
+++ python/llvm_pi.py
@@ -7,14 +7,28 @@ class LLVMTypes:
     BOOL = ir.IntType(1)
     VOID = ir.VoidType()
 
+class Node:
+    opr = []
+    def __init__(self):
+        self.opr.append(ir.Constant(LLVMTypes.INT, 42))
+        self.opr.append(ir.Constant(LLVMTypes.INT, 2))
+
 class LLVMExp():
     def __init__(self, function):
         self.function = function
         self.block = function.append_basic_block(name="entry")
         self.builder = ir.IRBuilder(self.block)
 
+    def compile(self, node):
+        self.compileSum(node)
+        print(self.function)
+
     def compileSum(self, node):
-        pass
+        lhs = node.opr[0]
+        rhs = node.opr[1]
+        self.builder.add(lhs, rhs)
+
+
     def compileSub(self, node):
         pass
     def compileMul(self, node):
@@ -23,6 +37,16 @@ class LLVMExp():
         pass
     def compileNot(self, node):
         pass
+
+
+module = ir.Module('main_module')
+func_type = ir.FunctionType(LLVMTypes.INT, [], False)
+lfunc = ir.Function(module, func_type, "main_function")
+
+LLVMExp(lfunc).compile(Node())
+
+print(module)
+
 
 
 
