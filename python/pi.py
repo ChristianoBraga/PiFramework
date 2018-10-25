3a2d29af7acd2ff21970bdd3af0f8f2cd7ba6729
Fernando Mendes <fjmendes1994@gmail.com>
2018-10-24 23:04:51 -0300

    Corrigindo pi notebook e pi.py

--- python/pi.py
+++ python/pi.py
@@ -1,21 +1,6 @@
 # <htmlcell>
 # <!-- pi.py with Jupyter Notebook markups. Run py2nb.py to generate a pi notebook. -->
 
-from nbformat import v3, v4
-
-with open("pi.py") as fpin:
-    text = fpin.read()
-
-nbook = v3.reads_py(text)
-nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
-
-jsonform = v4.writes(nbook) + "\n"
-with open("pi.ipynb", "w") as fpout:
-    fpout.write(jsonform)
-
-
-### pi.py com markups para notebook
-
 # coding: utf-8
 
 # <markdowncell>
