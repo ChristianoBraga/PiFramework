65ad4cda8a2a527919ee626b8c8e2c5cb714789d
ChristianoBraga <cbraga@ic.uff.br>
2019-06-14 10:54:28 -0300

    Organizing python folder for imp2 and travis.

--- python/impiler.py
+++ python/impiler.py
@@ -4,114 +4,98 @@
 import pi
 
 class Impiler(object):
-    def paren_exp(self, ast):
-        return ast.e
-    
     def identifier(self, ast):
         return pi.Id(str(ast))
 
     def number(self, ast):
         return pi.Num(int(ast))
 
-    def un_exp(self, ast):
-        if ast.op == "not":
-            return pi.Not(ast.e)
-        
-    def bin_exp(self, ast):
-        if ast.op == "+":
-            return pi.Sum(ast.e1, ast.e2)
-        elif ast.op == "-":
-            return pi.Sub(ast.e1, ast.e2)        
-        elif ast.op == "*":
-            return pi.Mul(ast.e1, ast.e2)        
-        elif ast.op == "/":
-            return pi.Div(ast.e1, ast.e2)        
-        elif ast.op == "and":
-            return pi.And(ast.e1, ast.e2)        
-        elif ast.op == "or":
-            return pi.Or(ast.e1, ast.e2)        
-        elif ast.op == "and":
-            return pi.And(ast.e1, ast.e2)        
-        elif ast.op == "<":
-            return pi.Lt(ast.e1, ast.e2)        
-        elif ast.op == "<=":
-            return pi.Le(ast.e1, ast.e2)        
-        elif ast.op == ">":
-            return pi.Gt(ast.e1, ast.e2)        
-        elif ast.op == ">=":
-            return pi.Ge(ast.e1, ast.e2)        
-        elif ast.op == "==":
-            return pi.Eq(ast.e1, ast.e2)        
-    
+    def addition(self, ast):
+        return pi.Sum(ast.left, ast.right)
+
+    def subtraction(self, ast):
+        return pi.Sub(ast.left, ast.right)
+
+    def multiplication(self, ast):
+        return pi.Mul(ast.left, ast.right)
+
+    def division(self, ast):
+        return pi.Div(ast.left, ast.right)
+
     def truth(self, ast):
         return pi.Boo(bool(ast))
 
-    def assign(self, ast):
-        return pi.Assign(ast.idn, ast.e)
+    def negation(self, ast):
+        return pi.Not(ast.b)    
 
-    def const(self, ast):
-        return pi.Bind(ast.idn, ast.e) 
+    def equality(self, ast):
+        return pi.Eq(ast.left, ast.right)
+
+    def lowerthan(self,ast):
+        return pi.Lt(ast.left, ast.right)
+
+    def greaterthan(self,ast):
+        return pi.Gt(ast.left, ast.right)
+
+    def lowereq(self,ast):
+        return pi.Le(ast.left, ast.right)
+
+    def greatereq(self,ast):
+        return pi.Ge(ast.left, ast.right)
+
+    def conjunction(self, ast):
+        return pi.And(ast.left, ast.right)
+
+    def disjunction(self, ast):
+        return pi.Or(ast.left, ast.right)
+
+    def nop(self, ast):
+        return pi.Nop()
+
+    def assign(self, ast):
+        return pi.Assign(ast.id, ast.e)
+
+    def cnst(self, ast):
+        return pi.Bind(ast.id, ast.e)
 
     def var(self, ast):
-        if isinstance(ast.idn, list):
-            bind = pi.Bind(ast.idn[0], pi.Ref(ast.e[0]))
-            for i in range(1, len(ast.idn)):
-                bind = pi.DSeq(bind, pi.Bind(ast.idn[i], pi.Ref(ast.e[0])))
-            return bind
-        else:
-            return pi.Bind(ast.idn, pi.Ref(ast.e))
+        return pi.Bind(ast.id, pi.Ref(ast.e))
 
-    def skip(self, ast):
-        return pi.Nop() 
-
-    def decSeq(self, ast):
-        if ast:
-            if isinstance(ast.d, list):
-                bind = ast.d[0]
-                for e in ast.d[1:]:
-                    bind = pi.DSeq(bind, e)
-                return bind
-            else:
-                return ast.d
-
-    def blk(self, ast):
-        if ast.ds:
-            if ast.cs:
-                return pi.Blk(ast.ds, ast.cs)
-            else:
-                return pi.Blk(ast.ds, pi.Nop())
-        else:
-            if ast.cs:
-                return pi.Blk(ast.cs)
-            else:
-                return pi.Blk(pi.Nop())
-        
-    def start(self, ast):
-        return self.blk(ast)
-
-    def cmd_seq(self, ast):
-        if isinstance(ast.ac, list):
-            cs = ast.ac[0]
-            for c in ast.ac[1:]:
-                cs = pi.CSeq(cs, c)
-            return cs
-        else:
-            return ast.ac
-        
     def let(self, ast):
-        return pi.Blk(ast.ds, ast.c)
+        if isinstance(ast.c, pi.Cmd):
+            return pi.Blk(ast.d, ast.c)
+        elif isinstance(ast.c, list):
+            cmd = ast.c[0]
+            for i in range(1, len(ast.c)):
+                cmd = pi.CSeq(cmd, ast.c[i])
+            return pi.Blk(ast.d, cmd)
 
     def loop(self, ast):
-        return pi.Loop(ast.t, ast.b)
-
-    def cond(self, ast):
-        if ast.b2:
-            return pi.Cond(ast.t, ast.b1, ast.b2)
+        if isinstance(ast.c, pi.Cmd):
+            return pi.Loop(ast.e, ast.c)
         else:
-            return pi.Cond(ast.t, ast.b1, pi.Nop())        
+            cmd = ast.c[0]
+            for i in range(1, len(ast.c)):
+                cmd = pi.CSeq(cmd, ast.c[i])
+            return pi.Loop(ast.e, cmd)
 
+        
+    def cond(self, ast):
+        cmd1 = ast.c1
+        cmd2 = ast.c2
+
+        if not(isinstance(cmd1, pi.Cmd)):
+            for i in range(1, len(ast.c1)):
+                cmd1 = pi.CSeq(cmd1, ast.c1[i])
+
+        if not(isinstance(cmd2, pi.Cmd)):
+            for i in range(1, len(ast.c2)):
+                cmd2 = pi.CSeq(cmd2, ast.c2[i])
+
+        return pi.Cond(ast.e, cmd1, cmd2)
+        
+        
     def __makeAbs(self, f, c):
-        assert(isinstance(f, list))
         if isinstance(c, pi.Blk):
             body = c
         else:
@@ -120,16 +104,19 @@ class Impiler(object):
         if f == []:
             return pi.Abs(pi.Formals(), body)
         else:
-            # Tatsu roduces list of identifiers and commas from
-            # formals = ','%{ identifiers }
-            formals = [e for e in f if e != ','] 
+            formals = []
+            for k,v in f.items():
+                if not v == None:
+                    formals.append(v)
             return pi.Abs(formals, body)
 
     def fn(self, ast):
-        body = self.__makeAbs(ast.f, ast.b)
-        return pi.BindAbs(ast.idn, body)
+        abs = self.__makeAbs(ast.f, ast.c)
+        return pi.BindAbs(ast.id, abs)
 
     def call(self, ast):
-        actuals = [e for e in ast.a if e != ','] 
-        aux = pi.Call(ast.idn, actuals)
-        return aux 
+        actuals = []
+        for k,v in ast.a.items():
+            if not v == None:
+                actuals.append(v)
+        return pi.Call(ast.i, actuals)
