d81f5ebc9af7fa3a1acfe1d0c827820ec89c9ee8
Christiano Braga <cbraga@ic.uff.br>
2020-07-10 17:38:11 -0300

    Recovering...

--- PiFramework/python/imp2/out/tatsu/__init__.pyi
+++
@@ -1,2 +0,0 @@
-from tatsu._config import __toolname__ as __toolname__
-from tatsu.tool import compile as compile, main as main, parse as parse, to_python_model as to_python_model, to_python_sourcecode as to_python_sourcecode
