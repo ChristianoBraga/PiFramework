1459e74e149604672d3f570f8d2fe08b172f0d26
Christiano Braga <cbraga@ic.uff.br:wq>
2021-04-07 15:41:18 -0300

    test.sh updated.

--- python/imp2/test.sh
+++ python/imp2/test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-for i in examples/*.imp2 ; do echo === ; echo $i ; echo ===; python3 imp2.py -f $i --last 2 -s --out ; done
+if [ $# -eq 0 ]
+then
+    echo "Use: test.sh n" ;
+    echo "Parameter n is the number of the state to be printed, counting backwards." ;
+    exit 1 ;
+fi
+for i in examples/*.imp2 ; do
+    echo === ;
+    echo $i ;
+    echo ===;
+    python3 imp2.py -f $i --last $1 -s --out ;
+done
